92ea5f7d8a2ede80df2c2767ab9ab19c7c9cd789
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2023-10-10 10:43:23 +0100

    day 01 complete
    everything work correct and good thanks to all the involved

---
+++ src/main/java/br/com/danielcruz/todolist/user/UserController.java
@@ -0,0 +1,33 @@
+package br.com.danielcruz.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Modificador
+ * puclic
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    /*
+     * Tipos
+     * String(Texto)
+     * Integer(Int) numeros inteiros
+     * Double(double) numeros decimais
+     * Float(float) numeros decimais menores
+     * Char (A C)
+     * Date (data)
+     * Void (Sem retorno)
+     */
+    //Vem de dentro do body da requisicao
+    @PostMapping("/")
+    public Void create(@RequestBody UserModel userModel){
+        System.out.println(userModel.name);
+        return null;
+    }
+}
